a51f5c872cbba4df60ce0f7d3ab48bb1e7744f70
tanlocnguyen151203 <tanlocnguyen12003@gmail.com>
2023-12-09 15:47:38 +0700

    Update kiemtra.js

--- src/kiemtra.js
+++ src/kiemtra.js
@@ -1,11 +1,11 @@
 
 const util = require('util');
-const { tong, hieu, tich, thuong } = require('./pheptoan'); // Chỉnh sửa đường dẫn tệp pheptoan.js
+const {PhepToan} = require('./pheptoan'); // Chỉnh sửa đường dẫn tệp pheptoan.js
 
 let soA = 10;
 let soB = 15;
 
-console.log(util.format(`Kết quả của phép cộng ${soA} + ${soB} là: ${tong(soA, soB)}`));
-console.log(util.format(`Kết quả của phép trừ ${soA} - ${soB} là: ${hieu(soA, soB)}`));
-console.log(util.format(`Kết quả của phép nhân ${soA} * ${soB} là: ${tich(soA, soB)}`));
-console.log(util.format(`Kết quả của phép chia ${soA} / ${soB} là: ${thuong(soA, soB)}`));
+console.log(util.format(`Kết quả của phép cộng ${soA} + ${soB} là: ${PhepToan.tong(soA, soB)}`));
+// console.log(util.format(`Kết quả của phép trừ ${soA} - ${soB} là: ${hieu(soA, soB)}`));
+// console.log(util.format(`Kết quả của phép nhân ${soA} * ${soB} là: ${tich(soA, soB)}`));
+// console.log(util.format(`Kết quả của phép chia ${soA} / ${soB} là: ${thuong(soA, soB)}`));
