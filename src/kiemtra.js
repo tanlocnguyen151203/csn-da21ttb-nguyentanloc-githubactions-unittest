c70b7f9a3f956a907551711b56b37f59f478710a
tanlocnguyen151203 <tanlocnguyen12003@gmail.com>
2023-12-11 09:24:33 +0700

    test1

--- src/kiemtra.js
+++ src/kiemtra.js
@@ -1,5 +1,18 @@
-const util = require( 'util');
-const { PhepToan } = require("./pheptoan");
+// const util = require( 'util');
+// const { PhepToan } = require("./pheptoan");
+
+// let soA = 200;
+// let soB = 100;
+
+
+// console.log(util.format(`Kết quả của phép cộng ${soA} + ${soB} là: ${PhepToan.tong(soA, soB)}`));
+// console.log(util.format(`Kết quả của phép trừ ${soA} - ${soB} là: ${PhepToan.hieu(soA, soB)}`));
+// console.log(util.format(`Kết quả của phép nhân ${soA} * ${soB} là: ${PhepToan.tich(soA, soB)}`));
+// console.log(util.format(`Kết quả của phép chia ${soA} / ${soB} là: ${PhepToan.thuong(soA, soB)}`));
+// console.log(util.format(`Kết quả của phép cân bậc ${soA} là: ${PhepToan.canBacHai(soA)}`));
+
+
+const PhepToan = require('../pheptoan');
 
 const a = 3;
 const b = 5;
