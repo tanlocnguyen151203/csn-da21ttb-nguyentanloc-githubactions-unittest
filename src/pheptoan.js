2a7e1bb803bafb57c584c8444a57197233a1d915
tanlocnguyen151203 <tanlocnguyen12003@gmail.com>
2023-12-12 10:22:49 +0700

    test

--- src/pheptoan.js
+++ src/pheptoan.js
@@ -1,36 +1,70 @@
+// const PhepToan = {
+//     tong: function(a, b) {
+//         a = parseFloat(a);
+//         b = parseFloat(b);
+//         let c = a + b;
+//         if (isNaN(c)) {
+//             console.log("Số nhập vào không phải là số");
+//         }
+//         return c;
+//     },
+//     hieu: function(a, b) {
+//         let c = a - b;
+//         return c;
+//     },
+//     tich: function(a, b) {
+//         let c = a * b;
+//         return c;
+//     },
+//     thuong: function(a, b) {
+//         if (b === 0) {
+//             return "Không thể chia cho 0";
+//         } else {
+//             let c = a / b;
+//             return c;
+//         }
+//     },
+//     canBacHai: function(number) {
+//         if (number < 0) {
+//             return "Không thể tính căn bậc hai của số âm";
+//         } else {
+//             return Math.sqrt(number);
+//         }
+//     }
+
+// };
+// module.exports = { PhepToan };
+
+
 const PhepToan = {
     tong: function(a, b) {
         a = parseFloat(a);
         b = parseFloat(b);
-        let c = a + b;
-        if (isNaN(c)) {
-            console.log("Số nhập vào không phải là số");
+        
+        if (isNaN(a) || isNaN(b)) {
+            throw new Error("Vui lòng nhập số hợp lệ");
         }
-        return c;
+
+        return a + b;
     },
     hieu: function(a, b) {
-        let c = a - b;
-        return c;
+        return a - b;
     },
     tich: function(a, b) {
-        let c = a * b;
-        return c;
+        return a * b;
     },
     thuong: function(a, b) {
         if (b === 0) {
-            return "Không thể chia cho 0";
-        } else {
-            let c = a / b;
-            return c;
+            throw new Error("Không thể chia cho 0");
         }
+        return a / b;
     },
     canBacHai: function(number) {
         if (number < 0) {
-            return "Không thể tính căn bậc hai của số âm";
-        } else {
-            return Math.sqrt(number);
+            throw new Error("Không thể tính căn bậc hai của số âm");
         }
+        return Math.sqrt(number);
     }
-
 };
+
 module.exports = { PhepToan };
